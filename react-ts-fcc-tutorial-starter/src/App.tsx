45775135c9327c621e29ae9bd327c0bc360f4be7
HiteshBhasin <77706673+HiteshBhasin@users.noreply.github.com>
2023-07-17 10:06:57 -0500

    1 file modified, 1 added, 1 deleted

--- react-ts-fcc-tutorial-starter/src/App.tsx
+++ react-ts-fcc-tutorial-starter/src/App.tsx
@@ -1,12 +1,12 @@
-import { TodoList, AddTodo } from './components'
-import { Toaster } from 'react-hot-toast'
+import { TodoList, AddTodo } from './components';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-5xl font-bold">Todo App Tutorial</h1>
+    <div className="App">
+      <Toaster position="button-center" />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
