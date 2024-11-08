711148d88303975e5b184af8be470735a190b3c3
Hitesh Bhasin <77706673+HiteshBhasin@users.noreply.github.com>
2024-11-08 16:50:39 -0600

    new commit

--- react-ts-fcc-tutorial-starter/src/components/AddTodo.tsx
+++
@@ -1,8 +0,0 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { toast } from 'react-hot-toast'
-import { useTodo } from '../context'
-import { Input } from './Input'
-
-export const AddTodo = () => {
-  return <div>Add To Do</div>
-}
