accfb0247e9d812f5ca11268e6ba3d4d634788bc
Hitesh Bhasin <77706673+HiteshBhasin@users.noreply.github.com>
2025-04-21 18:11:05 +0000

    new commit

---
+++ portforlio-App/Client/Component/contactForm.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+
+
+function MyFrom(){
+    const [name, setName] = useState(' ');
+    const [email, setEmail] = useState(' ');
+    
+    const handleSubmit = 
+
+        return
+
+}
