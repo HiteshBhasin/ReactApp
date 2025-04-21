bae3750aff6b6095d957afbe84770351525c82b6
Hitesh Bhasin <77706673+HiteshBhasin@users.noreply.github.com>
2025-04-21 17:37:17 +0000

    new commit

--- portforlio-App/Client/client/src/App.js
+++ portforlio-App/Client/client/src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
-import {useStae} from "react";
+import {useState, useEffect} from "react";
 import './App.css';
 
 function App() {
+const [data, setData] = useState(null);
+useEffect(()=>{
+fetch("/api")
+.then((res)=>res.json())
+.then((data)=>setData(data.message()));
+},[]);
+
+
   return (
     <div className="App">
     <header className="App-header">
