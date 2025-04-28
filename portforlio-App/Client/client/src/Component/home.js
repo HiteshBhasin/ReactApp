51c123946aa318dd8850e1d0ae0b1931b7d3b7bb
Hitesh Bhasin <77706673+HiteshBhasin@users.noreply.github.com>
2025-04-28 14:14:10 +0000

    new commit

--- portforlio-App/Client/client/src/Component/home.js
+++ portforlio-App/Client/client/src/Component/home.js
@@ -1,9 +1,26 @@
 import React from 'react';
-
+import { useState } from 'react';
+import './darkTheme.css';
 const Home = ()=>{
-<h1>
-    WELCOME!
-</h1>
+const [theme, setTheme] = useState('light');
+
+const toggeleTheme =()=>{
+    setTheme(theme==='light'?'dark':'light');
+}
+
+return(<div className={`home ${theme}`}>
+    <h1>
+        WELCOME!
+    </h1> <br />
+    <h2>
+        Select you theme.
+        <button onClick={toggeleTheme}>
+    Switch to {theme==='light'? 'Dark':'Light'}
+        </button>
+    </h2>
+</div>)
+
+
 }
 
 export default Home;
